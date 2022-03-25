4a608640c0d3bd56b8401e976a112b9d92747e17
jofbob <jofbob@hotmail.com>
2022-03-25 11:32:33 +1100

    blog post changes

---
+++ js/register.js
@@ -0,0 +1,13 @@
+
+let title = document.querySelector(".form-title");
+let form = document.querySelector(".login-form");
+
+let registerButton = document.querySelector(".register-button")
+registerButton.addEventListener('click', event => {
+    title.innerHTML = "Register";
+
+    let email = document.createElement("input");
+    email.setAttribute("type", "email");
+    
+});
+
