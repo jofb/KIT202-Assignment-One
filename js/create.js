1b5c7bf7c3551236de5cd156ee10b55cdb308117
Fin <finlay@mckenziecorp.com.au>
2022-03-31 11:34:36 +1100

    create post icon

--- js/create.js
+++ js/create.js
@@ -4,6 +4,8 @@ const desiredTitle = document.getElementById("post-title");
 const postPreview = document.getElementById("body-preview");
 const desiredPost = document.getElementById("post-body");
 
+// These two functions are called by the onchange event from their respective form elements.
+// Only for the purpose of previewing a post before it is submitted
 function updateTitle() {
     titlePreview.innerText = desiredTitle.value;
 }
