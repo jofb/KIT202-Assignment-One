f57b9b98dc29725b3600d8e7dd9654170e6f6234
jordanwylde-browne <jofbob@hotmail.com>
2022-03-31 12:02:16 +1100

    create post validation/datetime

--- js/create.js
+++ js/create.js
@@ -1,8 +1,11 @@
 const titlePreview = document.getElementById("title-preview");
-const desiredTitle = document.getElementById("post-title");
-
 const postPreview = document.getElementById("body-preview");
+const datePreview = document.getElementById("date-preview");
+
+const desiredTitle = document.getElementById("post-title");
 const desiredPost = document.getElementById("post-body");
+
+const form = document.forms['create-form'];
 
 function updateTitle() {
     titlePreview.innerText = desiredTitle.value;
@@ -11,3 +14,29 @@ function updateTitle() {
 function updatePostBody() {
     postPreview.innerText = desiredPost.value;
 }
+
+function updateDate() {
+    const d = new Date();
+    const monthArray = 
+    ["January","February","March","April","May","June",
+    "July","August","September","October","November","December"];
+
+    let day = String(d.getDate());
+    let month = monthArray[d.getMonth()];
+    let year = d.getFullYear();
+    let daySuffix = 'th';
+    if (day.charAt(day.length - 1) == 3) {
+        daySuffix = 'rd';
+    }
+    else if (day.charAt(day.length - 1) == 2) {
+        daySuffix = 'nd';
+    }
+    else if (day.charAt(day.length - 1) == 1) {
+        daySuffix = 'st';
+    }
+
+    let dateString = `${day}${daySuffix} ${month} ${year}`;
+    datePreview.textContent = dateString;
+}
+
+updateDate();
